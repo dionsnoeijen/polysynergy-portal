294c994a86ba7ecb0818d42ca5a9e839d5edf8db
Dion Snoeijen <hallo@dionsnoeijen.nl>
2025-01-08 16:20:32 +0100

    Fixed env var naming

--- src/contexts/auth-context.tsx
+++ src/contexts/auth-context.tsx
@@ -4,8 +4,8 @@ import { AuthProvider } from "react-oidc-context";
 import React from "react";
 
 const cognitoAuthConfig = {
-    authority: process.env.NEXT_PUBLIC_COGNITO_AUTHORITY,
-    client_id: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID,
+    authority: process.env.NEXT_PUBLIC_AWS_COGNITO_AUTHORITY,
+    client_id: process.env.NEXT_PUBLIC_AWS_COGNITO_CLIENT_ID,
     redirect_uri: process.env.NEXT_PUBLIC_AWS_COGNITO_REDIRECT_URL,
     response_type: "code",
     scope: "phone openid email",

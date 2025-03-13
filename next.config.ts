e8013f0a92275b85072cf4cbef1ec96b819f0416
Dion Snoeijen <hallo@dionsnoeijen.nl>
2025-03-13 12:27:05 +0100

    Make variables available

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
     devIndicators: {
         appIsrStatus: false, // Disable the "isr" indicator (icon bottom left during dev)
     },
+    env: {
+        NEXT_PUBLIC_AWS_COGNITO_AUTHORITY: process.env.NEXT_PUBLIC_AWS_COGNITO_AUTHORITY,
+        NEXT_PUBLIC_AWS_COGNITO_DOMAIN: process.env.NEXT_PUBLIC_AWS_COGNITO_DOMAIN,
+        NEXT_PUBLIC_AWS_COGNITO_CLIENT_ID: process.env.NEXT_PUBLIC_AWS_COGNITO_CLIENT_ID,
+        NEXT_PUBLIC_AWS_COGNITO_LOGOUT_URL: process.env.NEXT_PUBLIC_AWS_COGNITO_LOGOUT_URL,
+        NEXT_PUBLIC_AWS_COGNITO_REDIRECT_URL: process.env.NEXT_PUBLIC_AWS_COGNITO_REDIRECT_URL,
+        NEXT_PUBLIC_POLYSYNERGY_API: process.env.NEXT_PUBLIC_POLYSYNERGY_API,
+    },
 };
 
 export default nextConfig;
